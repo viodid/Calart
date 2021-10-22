c708b1947092ab0925801e65cdd5533c79b20a27
viodid <david.yunta.aller@gmail.com>
2021-10-22 12:58:29 +0200

    create db, apology error and registered button in login

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+from cs50 import SQL
+
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///calart.db")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/social")
+def social():
+    return render_template("social.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template(
+                "apology.html", top=403, bottom="Debes añadir tu correo"
+            )
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template(
+                "apology.html", top=403, bottom="Debes añadir tu contraseña"
+            )
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        # Provide a username in the session
+        session["username"] = rows[0]["username"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    return render_template("login.html")
