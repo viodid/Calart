f2eebf136e2661394363655ca890ac7dfecf2a90
viodid <david.yunta.aller@gmail.com>
2021-11-09 20:38:18 +0100

    change config file

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 from os import urandom
+import json
 
-SECRET_KEY = "6478264767dg"
+with open("/etc/config.json") as config_file:
+	config = json.load(config_file)
+
+
+SECRET_KEY = config.get("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
